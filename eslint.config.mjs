23b99a2254ae30b3284ca1317a6002b24a2733cb
ccrsxx <aminrisal@gmail.com>
2025-07-13 12:08:18 +0700

    feat: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,96 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    ignores: ['*.mjs']
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
+      }
+    },
+    settings: {
+      'import/resolver': {
+        node: true,
+        typescript: true
+      }
+    },
+    rules: {
+      // General rules
+      semi: ['error', 'always'],
+      quotes: [
+        'error',
+        'single',
+        {
+          avoidEscape: true
+        }
+      ],
+      'prefer-const': [
+        'error',
+        {
+          destructuring: 'all'
+        }
+      ],
+      'jsx-quotes': ['error', 'prefer-single'],
+      'linebreak-style': ['error', 'unix'],
+      'no-console': 'warn',
+      'comma-dangle': ['error', 'never'],
+      'no-unused-expressions': 'error',
+      'no-constant-binary-expression': 'error',
+
+      // Import plugin rules
+      'import/order': [
+        'warn',
+        {
+          warnOnUnassignedImports: true,
+          pathGroupsExcludedImportTypes: ['type'],
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type'
+          ]
+        }
+      ],
+      'import/no-duplicates': ['warn', { 'prefer-inline': true }],
+      'import/consistent-type-specifier-style': ['warn', 'prefer-inline'],
+
+      // TypeScript plugin rules
+      '@typescript-eslint/consistent-type-imports': 'warn',
+      '@typescript-eslint/consistent-type-exports': 'warn',
+      '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ],
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        {
+          checksVoidReturn: {
+            attributes: false
+          }
+        }
+      ]
+    }
+  }
+);
