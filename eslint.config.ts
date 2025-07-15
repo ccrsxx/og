aad27336a2bccd7657de8be6f9dc46ba8d358f34
Ami <aminrisal@gmail.com>
2025-07-16 04:20:55 +0700

    refactor: use .ts for eslint config file

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,10 @@
-// @ts-check
-
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   eslint.configs.recommended,
+  // eslint-disable-next-line import/no-named-as-default-member
   tseslint.configs.recommendedTypeChecked,
   {
     ignores: ['*.mjs']
