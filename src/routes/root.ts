0060086688c92471606a99c9edac99d0b00cbc09
Ami <aminrisal@gmail.com>
2025-07-16 11:14:00 +0700

    fix: change root route method from use to get for proper handling

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,5 +2,5 @@ import { type Application } from 'express';
 import { RootController } from '../controllers/root.ts';
 
 export default (app: Application): void => {
-  app.use('/', RootController.ping);
+  app.get('/', RootController.ping);
 };
