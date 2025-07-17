5982185ee05c9c89753cd1a165763987a92e0968
Ami <aminrisal@gmail.com>
2025-07-17 23:15:15 +0700

    feat: supports multi client sse broadcast

--- src/controllers/spotify.ts
+++ src/controllers/spotify.ts
@@ -1,5 +1,5 @@
 import { SpotifyService } from '../services/spotify.ts';
-import { logger } from '../loaders/pino.ts';
+import { SpotifySSEService } from '../services/spotify-sse.ts';
 import type { Request, Response } from 'express';
 import type { ApiResponse } from '../utils/types/api.ts';
 import type { CurrentlyPlaying } from '../utils/types/spotify.ts';
@@ -9,54 +9,13 @@ async function getCurrentlyPlaying(
   res: Response<ApiResponse<CurrentlyPlaying>>
 ): Promise<void> {
   const currentlyPlaying = await SpotifyService.getCurrentlyPlaying();
-
   res.status(200).json({
     data: currentlyPlaying
   });
 }
 
-function getCurrentlyPlayingSSE(_req: Request, res: Response): void {
-  // Interval for refreshing the currently playing track
-  const SSE_INTERVAL = 3000;
-
-  res.writeHead(200, {
-    Connection: 'keep-alive',
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache'
-  });
-
-  async function handleSendServerSentEvent(): Promise<void> {
-    try {
-      const currentlyPlaying = await SpotifyService.getCurrentlyPlaying();
-
-      const jsonString = JSON.stringify({
-        data: currentlyPlaying
-      });
-
-      const eventString = `data: ${jsonString}\n\n`;
-
-      res.write(eventString);
-    } catch (err) {
-      const errorMessage = err instanceof Error ? err.message : 'Unknown error';
-
-      logger.error(
-        err,
-        `Error while fetching currently playing track: ${errorMessage}`
-      );
-    }
-  }
-
-  void handleSendServerSentEvent();
-
-  const intervalId = setInterval(
-    () => void handleSendServerSentEvent(),
-    SSE_INTERVAL
-  );
-
-  res.on('close', () => {
-    clearInterval(intervalId);
-    res.end();
-  });
+function getCurrentlyPlayingSSE(req: Request, res: Response): void {
+  SpotifySSEService.handleConnection(req, res);
 }
 
 export const SpotifyController = {
---
+++ src/services/spotify-sse.ts
@@ -0,0 +1,107 @@
+import { SpotifyService } from '../services/spotify.ts';
+import { logger } from '../loaders/pino.ts';
+import type { Request, Response } from 'express';
+
+type SSEClient = {
+  id: string;
+  res: Response;
+};
+
+type SSEState = {
+  clients: SSEClient[];
+  pollingIntervalId: NodeJS.Timeout | null;
+};
+
+const SSEStates: SSEState = {
+  clients: [],
+  pollingIntervalId: null
+};
+
+const SSE_INTERVAL = 1000; // 1 seconds
+
+async function pollAndBroadcast(SSEClient?: SSEClient): Promise<void> {
+  try {
+    const currentlyPlaying = await SpotifyService.getCurrentlyPlaying();
+
+    const jsonString = JSON.stringify({
+      data: currentlyPlaying
+    });
+
+    const eventString = `data: ${jsonString}\n\n`;
+
+    // If SSEClient is provided, send the event only to that client. Happens when a new client connects.
+    if (SSEClient) {
+      logger.info('Broadcasting first event to new client.');
+      SSEClient.res.write(eventString);
+    } else {
+      if (!SSEStates.clients.length) {
+        logger.info('No clients connected. Skipping broadcast.');
+        return;
+      }
+
+      logger.info(`Broadcasting event to ${SSEStates.clients.length} clients.`);
+
+      SSEStates.clients.forEach((client) => client.res.write(eventString));
+    }
+  } catch (err) {
+    const errorMessage = err instanceof Error ? err.message : 'Unknown error';
+    logger.error(
+      err,
+      `Error during Spotify poll and broadcast: ${errorMessage}`
+    );
+  }
+}
+
+function handleConnection(_req: Request, res: Response): void {
+  res.writeHead(200, {
+    Connection: 'keep-alive',
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache'
+  });
+
+  const SSEClient: SSEClient = {
+    id: crypto.randomUUID(),
+    res
+  };
+
+  SSEStates.clients.push(SSEClient);
+
+  logger.info(
+    `Client connected: ${SSEClient.id}. Total clients: ${SSEStates.clients.length}`
+  );
+
+  // If this new client, we send send thh first event immediately.
+  void pollAndBroadcast(SSEClient);
+
+  if (!SSEStates.pollingIntervalId) {
+    logger.info('First client connected. Starting polling interval.');
+
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    SSEStates.pollingIntervalId = setInterval(pollAndBroadcast, SSE_INTERVAL);
+  }
+
+  res.on('close', () => {
+    SSEStates.clients = SSEStates.clients.filter(
+      (client) => client.id !== SSEClient.id
+    );
+
+    logger.info(
+      `Client disconnected: ${SSEClient.id}. Total clients: ${SSEStates.clients.length}`
+    );
+
+    // If there are no more clients connected, stop the polling interval.
+    if (!SSEStates.clients.length && SSEStates.pollingIntervalId) {
+      logger.info('Last client disconnected. Stopping polling interval.');
+
+      clearInterval(SSEStates.pollingIntervalId);
+
+      SSEStates.pollingIntervalId = null;
+    }
+
+    res.end();
+  });
+}
+
+export const SpotifySSEService = {
+  handleConnection
+};
