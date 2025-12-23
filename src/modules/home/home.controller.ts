235c58d7543eaa5e57f53318e086005f3cbbf2e8
Ami <ami@ccrsxx.com>
2025-12-23 19:11:52 +0700

    fix(home): update ping message to indicate successful response

--- src/modules/home/home.controller.ts
+++ src/modules/home/home.controller.ts
@@ -5,7 +5,7 @@ import type { ApiResponse } from '../../core/utils/types/api.ts';
 function ping(req: Request, res: Response<ApiResponse>): void {
   res.status(200).json({
     data: {
-      message: 'Welcome! The API is up and running',
+      message: 'Welcome! The API is up and running. Ping successful.',
       documentationUrl: `${getPublicUrlFromRequest(req)}/docs`
     }
   });
